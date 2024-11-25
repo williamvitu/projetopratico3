0f4ff7adc16c873fd398ff17c88d8bd20433a3c8
vitourwd <vitorwilliamd@gmail.com>
2024-11-24 23:06:50 -0300

    tests and improvements for icecream class

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -4,16 +4,26 @@ from src.models.restaurant import Restaurant
 class IceCreamStand(Restaurant):
     """Um tipo especializado de restaurante."""
 
-    def __init__(self, restaurant_name, cuisine_type, flavors_list):
+    # Melhoria, adicionar anotação dos atributos (Fixed)
+    def __init__(self, restaurant_name: str, cuisine_type: str, flavors_list: list[str]):
         """
         Inicialize os atributos da classe pai.
         Em seguida, inicialize os atributos específicos de uma sorveteria.
         """
+    # Melhoria, validação nos atributos da classe (fixed)
+        if not isinstance(restaurant_name, str):
+            raise TypeError("O atributo 'restaurant_name' deve ser uma string.")
+        if not isinstance(cuisine_type, str):
+            raise TypeError("O atributo 'cuisine_type' deve ser uma string.")
+        if not isinstance(flavors_list, list):
+            raise TypeError("O atributo 'flavors_list' deve ser uma lista de strings.")
+
         super().__init__(restaurant_name, cuisine_type)
         self.flavors = flavors_list
 
     def flavors_available(self):
         """Percorra a lista de sabores disponíveis e imprima."""
+        # Melhoria, validar se o restaurante está aberto e possui estoque (FIXED)
         if self.flavors:
             result = "\nNo momento temos os seguintes sabores de sorvete disponíveis:"
             for flavor in self.flavors:
--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -1,7 +1,35 @@
+from src.models.ice_cream_stand import IceCreamStand
+
+
 class TestIceCreamStand:
 
-    def test_flavors_available(self):
-        assert False
+    def test_flavors_available_success(self):
+        # Setup
+        test_flavors = ["Morango", "Chocolate", "Creme"]
+        icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        icecream.open_restaurant()
+
+        # Chamada
+        resultado = icecream.flavors_available()
+
+        # Validação
+        resultado_esperado = "\nNo momento temos os seguintes sabores de sorvete disponíveis:"
+        for flavor in test_flavors:
+            resultado_esperado += f"\t-{flavor}"
+
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
+
+    def test_flavors_out_of_stock(self):
+        # Setup
+        test_flavors = []
+        icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+
+        # Chamada
+        resultado = icecream.flavors_available()
+
+        # Validação
+        resultado_esperado = "Estamos sem estoque atualmente!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_find_flavor(self):
         assert False
