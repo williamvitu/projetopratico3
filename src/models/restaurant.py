93bb60f6e4e85ba8166546915b020f43c4863ea8
vitourwd <vitorwilliamd@gmail.com>
2024-11-24 23:06:25 -0300

    new tests for restaurant class, method bug fixes and improvements..

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -1,7 +1,15 @@
 class Restaurant:
     """Model de restaurante simples."""
 
-    def __init__(self, restaurant_name, cuisine_type):
+    # Melhoria, adicionar anotação dos atributos (Fixed)
+    def __init__(self, restaurant_name: str, cuisine_type: str):
+
+        # Melhoria, validação nos atributos da classe (fixed)
+        if not isinstance(restaurant_name, str):
+            raise TypeError("O atributo 'restaurant_name' deve ser uma string.")
+        if not isinstance(cuisine_type, str):
+            raise TypeError("O atributo 'cuisine_type' deve ser uma string.")
+
         self.restaurant_name = restaurant_name.title()
         self.cuisine_type = cuisine_type
         self.number_served = 0
@@ -9,15 +17,22 @@ class Restaurant:
 
     def describe_restaurant(self):
         """Imprima uma descrição simples da instância do restaurante."""
-        result = f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}."
-        result += f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto."
+        # Bug, está exibindo a mesma variável para ambas descrições, deveria ser restaurante_name e cuisine_type (Fixed)
+        # Bug, Palavras em inglês misturadas com palavras em português (Fixed)
+        # Bug, Palavra 'restaturante' ao invés de 'restaurante' (FIXED)
+        result = f"Esse restaurante chama {self.restaurant_name} e serve {self.cuisine_type}."
+
+        # Bug, Palavra 'restaturante' ao invés de 'restaurante' (FIXED)
+        result += f"Esse restaurante está servindo {self.number_served} consumidores desde que está aberto."
         return result
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
         if not self.open:
-            self.open = False
-            self.number_served = -2
+            # Bug, deveria ser essa atribuição -> self.open = True (FIXED)
+            self.open = True
+            # Bug, não faz sentido colocar number_served negativo, apenas remover essa linha. (Fixed)
+            #self.number_served = -2
             return f"{self.restaurant_name} agora está aberto!"
         else:
             return f"{self.restaurant_name} já está aberto!"
@@ -33,14 +48,26 @@ class Restaurant:
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
+
+        # Melhoria, Validar o tipo recebido, para não receber valores inválidos (FIXED)
+        if type(total_customers) is not int:
+            return "Parametro invalido"
+
         if self.open:
             self.number_served = total_customers
+
         else:
             return f"{self.restaurant_name} está fechado!"
 
-    def increment_number_served(self, more_customers):
+    def increment_number_served(self, more_customers : int):
         """Aumenta número total de clientes atendidos por este restaurante."""
+
+        # Melhoria Validar tipo de variável recebida (FIXED)
+        if type(more_customers) is not int:
+            return "Parametro invalido"
+
         if self.open:
-            self.number_served = more_customers
+            # Bug, deveria somar e não atribuir (FIXED)
+            self.number_served += more_customers
         else:
             return f"{self.restaurant_name} está fechado!"
--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -1,16 +1,144 @@
+from src.models.restaurant import Restaurant
+import pytest
+
 class TestRestaurant:
 
     def test_describe_restaurant(self):
-        assert False
+        # Setup
+        restaurant =  Restaurant("Seu Alfredo", "Comida Italiana")
+
+        # Chamada
+        resultado = restaurant.describe_restaurant()
+
+        # Validação
+        resultado_esperado = (f"Esse restaurante chama {restaurant.restaurant_name} e serve {restaurant.cuisine_type}."
+                              f"Esse restaurante está servindo {restaurant.number_served} consumidores desde que está aberto.")
+        assert resultado == resultado_esperado, f"Esperado a mensagem: {resultado_esperado}, recebido: {resultado}"
 
     def test_open_restaurant(self):
-        assert False
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+
+        # Chamada
+        resultado = restaurant.open_restaurant()
+
+        # Validação
+        resultado_esperado = f"{restaurant.restaurant_name} agora está aberto!"
+
+        assert resultado == resultado_esperado, f"Esperado a mensagem: {resultado_esperado}, recebido: {resultado}"
+        assert restaurant.open is True, f"Esperado: {True}, recebido: {restaurant.open}"
+
+    def test_open_restaurant_for_an_already_opened_restaurant(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+
+        # Chamada
+        resultado = restaurant.open_restaurant()
+
+        # Validação
+        resultado_esperado =  f"{restaurant.restaurant_name} já está aberto!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+        assert restaurant.open is True, f"Esperado: {True}, recebido: {restaurant.open}"
 
     def test_close_restaurant(self):
-        assert False
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
 
-    def test_set_number_served(self):
-        assert False
+        # Chamada
+        resultado = restaurant.close_restaurant()
 
-    def test_increment_number_served(self):
-        assert False
+        # Validação
+        resultado_esperado =  f"{restaurant.restaurant_name} agora está fechado!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+        assert restaurant.open is False, f"Esperado: {False}, recebido: {restaurant.open}"
+
+    def test_close_restaurant_for_an_already_closed_restaurant(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+
+        # Chamada
+        resultado = restaurant.close_restaurant()
+
+        # Validação
+        resultado_esperado = f"{restaurant.restaurant_name} já está fechado!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+        assert restaurant.open is False, f"Esperado: {False}, recebido: {restaurant.open}"
+
+    def test_set_number_served_success(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+
+        # Chamada
+        resultado = restaurant.set_number_served(10)
+
+        # Validação
+        resultado_esperado = 10
+        assert restaurant.number_served == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {restaurant.number_served}"
+
+    @pytest.mark.parametrize("test_data", ["HAHAHA", [], {}, 2.3, ""])
+    def test_set_number_served_invalid_parameter(self, test_data):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+
+        # Chamada
+        resultado = restaurant.set_number_served(test_data)
+
+        # Validação
+        resultado_esperado = "Parametro invalido"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+
+    def test_set_number_served_for_closed_restaurant(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+
+        # Chamada
+        resultado = restaurant.set_number_served(10)
+
+        # Validação
+        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+
+    def test_increment_number_served_success(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+        restaurant.set_number_served(10)
+        restaurant.increment_number_served(10)
+        # Chamada
+        resultado = restaurant.number_served
+
+        # Validação
+        resultado_esperado = 20
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+
+    def test_increment_number_served_closed_restaurant(self):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+        restaurant.set_number_served(10)
+        restaurant.close_restaurant()
+
+        # Chamada
+        resultado = restaurant.increment_number_served(10)
+
+        # Validação
+        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
+
+    @pytest.mark.parametrize("test_data", ["HAHAHA", [], {}, 2.3, ""])
+    def test_increment_number_served_invalid_parameter(self, test_data):
+        # Setup
+        restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        restaurant.open_restaurant()
+        restaurant.set_number_served(10)
+
+        # Chamada
+        resultado = restaurant.increment_number_served(test_data)
+
+        # Validação
+        resultado_esperado = "Parametro invalido"
+        assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
